1bb46818a67fb370a13d1c7f6b25483be07fadc9
Tony de Faria <tonydefaria@iigethr.com>
2019-03-26 09:57:28 +0000

    column divider

--- assets/javascripts/initializer.js
+++ assets/javascripts/initializer.js
@@ -31,17 +31,44 @@ bindEvents = function() {
   }
 };
 
+bindWrappers = function() {
+  var divs = $("div.articles div.articles-box div.articles-item");
+  var wrapper = "<div class='left auto'></div>";
+  var divWidth = $(".articles").width();
+
+  if (divWidth <= 360) {
+    for(var i = 0; i < divs.length; i+=2) {
+      divs.slice(i, i+2).wrapAll("<div class='left wide'></div>");
+    }
+  } else if (divWidth <= 480) {
+    for(var i = 0; i < divs.length; i+=2) {
+      divs.slice(i, i+2).wrapAll("<div class='left wide'></div>");
+    }
+  } else if (divWidth <= 720) {
+    for(var i = 0; i < divs.length; i+=3) {
+      divs.slice(i, i+3).wrapAll("<div class='left wide'></div>");
+    }
+  } else if (divWidth <= 960) {
+    for(var i = 0; i < divs.length; i+=4) {
+      divs.slice(i, i+4).wrapAll("<div class='left wide'></div>");
+    }
+  }
+}
+
 $(document).on( "turbolinks:load", function() {
   bindEvents();
+  bindWrappers();
   Turbolinks.clearCache();
 });
 
 $(window).resize(function() {
+  bindWrappers();
   $(".altai-panel").altaiPanel({
     trim: 112
   });
   var divHeight = $('.universal.main').innerHeight();
   $('.responsive-navigation').css('min-height', divHeight + 'px');
+  Turbolinks.clearCache();
 });
 
 $(window).trigger('resize');
